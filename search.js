a5b9105e0bbb2cdaf41c532e99fa252fb8156853
alexpkgs <alexzsk@outlook.com>
2024-09-14 18:52:00 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,13 +1,13 @@
-   document.getElementById('search-button').addEventListener('click', function() {
-            const query = document.getElementById('search-input').value;
-            if (query) {
-                // Construct the search URL for Startpage
-                const baseUrl = 'https://www.startpage.com/sp/search';
-                const searchUrl = `${baseUrl}?query=${encodeURIComponent(query)}`;
-                
-                // Redirect to the Startpage search results page
-                window.location.href = searchUrl;
-            } else {
-                alert('Please enter a search term.');
-            }
-        });
+document.getElementById('search-button').addEventListener('click', function() {
+    const query = document.getElementById('search-input').value;
+    if (query) {
+        // Construct the search URL for DuckDuckGo
+        const baseUrl = 'https://duckduckgo.com/';
+        const searchUrl = `${baseUrl}?q=${encodeURIComponent(query)}`;
+        
+        // Redirect to the DuckDuckGo search results page
+        window.location.href = searchUrl;
+    } else {
+        alert('Please enter a search term.');
+    }
+});
